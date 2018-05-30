4d6d16615a64756dce088c29091e69e491b8e5cd
Berényi Szabolcs <szabbi97@gmail.com>
2018-05-30 19:49:13 +0200

    jav

---
+++ FeladatFelado2/app/src/main/java/com/example/fzksb/feladatfelado/Database/User.java
@@ -0,0 +1,4 @@
+package com.example.fzksb.feladatfelado.Database;
+
+public class User {
+}
--- FeladatFelado2/app/src/main/java/com/example/fzksb/feladatfelado/View/MainActivity.java
+++ FeladatFelado2/app/src/main/java/com/example/fzksb/feladatfelado/View/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.fzksb.feladatfelado;
+package com.example.fzksb.feladatfelado.View;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.fzksb.feladatfelado.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
