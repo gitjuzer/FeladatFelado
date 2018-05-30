b34439ed8c66970477c4266249ad087334350e5e
FazekasBalint <fzksbalint@gmail.com>
2018-05-30 19:40:40 +0200

    class update

--- FeladatFelado2/app/src/main/java/com/example/fzksb/feladatfelado/Model/Student.java
+++ FeladatFelado2/app/src/main/java/com/example/fzksb/feladatfelado/Model/Student.java
@@ -4,16 +4,7 @@ import com.example.fzksb.feladatfelado.Model.Exam;
 
 import java.util.ArrayList;
 
-public class Student {
-    String username;
-    String password;
-    public String Username(){
-        return username;
-    }
-
-    public Boolean CheckPwd(String password){
-        return this.password == password;
-    }
+public class Student extends User {
 
     ArrayList<Exam> exams;
 }
--- FeladatFelado2/app/src/main/java/com/example/fzksb/feladatfelado/Model/Teacher.java
+++ FeladatFelado2/app/src/main/java/com/example/fzksb/feladatfelado/Model/Teacher.java
@@ -2,16 +2,7 @@ package com.example.fzksb.feladatfelado.Model;
 
 import java.util.ArrayList;
 
-public class Teacher {
-    String username;
-    String password;
-    public String Username(){
-            return username;
-    }
-
-    public Boolean CheckPwd(String password){
-        return this.password == password;
-    }
+public class Teacher extends User {
 
     ArrayList<Student> students;
 }
